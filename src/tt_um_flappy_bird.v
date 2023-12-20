9221df9ae3efa553b0c5e2ee64520a8f3ac6f94d
Default Application User <k12008704@students.jku.at>
2023-12-20 14:03:03 +0100

    defect-commit

--- src/tt_um_flappy_bird.v
+++ src/tt_um_flappy_bird.v
@@ -1,7 +1,7 @@
 `default_nettype none
 
-module flappy_bird 
-#( parameter MAX_COUNT = 24'd10_000_000 ) (
+module tt_um_flappy_bird 
+#( ) (
     input  wire [7:0] ui_in,    // Dedicated inputs - connected to the input switches
     output wire [7:0] uo_out,   // Dedicated outputs - connected to the 7 segment display
     input  wire [7:0] uio_in,   // IOs: Bidirectional Input path
@@ -12,12 +12,12 @@ module flappy_bird
     input  wire       rst_n     // reset_n - low to reset
 );
 
-    wire reset = ! rst_n;
+    reg reset = ! rst_n;
 
     // use bidirectionals as outputs
     assign uio_oe = 8'b11111111;
 	
-	parameter gs;
+	parameter gs = 8;
 	reg [gs*gs-1:0] my_disp;
 	reg e_disp ;
 	wire d_disp;
@@ -39,24 +39,35 @@ module flappy_bird
 		
 		
     // external clock is 10MHz, so need 24 bit counter
-    reg [23:0] second_counter;
-    reg [3:0] digit;
+    
+    
 
     // if external inputs are set then use that as compare count
     // otherwise use the hard coded MAX_COUNT
-    wire [23:0] compare = ui_in == 0 ? MAX_COUNT: {6'b0, ui_in[7:0], 10'b0};
+    
 	
     always @(posedge clk) begin
     
-		gs <= 8;
 		e_disp <= 1'b1;
-		my_disp <= {1,0,0,0,0,0,0,1,0,1,0,0,0,0,1,0
+		my_disp <= 64'b0;
+		my_disp[0] <= 1'b1;
+		my_disp[9] <= 1'b1; 
+		my_disp[18] <= 1'b1; 
+		my_disp[27] <= 1'b1; 
+		my_disp[36] <= 1'b1; 
+		my_disp[45] <= 1'b1; 
+		my_disp[54] <= 1'b1; 
+		my_disp[63] <= 1'b1; 
+		
+		
         if (d_disp == 1'b1)  begin
-		e_disp = 1'b0;
-	end
+			e_disp <= 1'b0;
+		end
+		if (reset) begin
+			e_disp <= 1'b1;
+		end
+		
     end
 
-    // instantiate segment display
-    seg7 seg7(.counter(digit), .segments(led_out));
-
+    
 endmodule
